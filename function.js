33bf76f9ca78fb764c8833bd000c13836a7f3381
Adarshjithu77 <adarshjithu77@gmail.com>
2025-07-03 17:14:04 +0530

    secondqstn done

--- function.js
+++ function.js
@@ -4,3 +4,12 @@ function sumLength(a,b){
     return a.length + b.length
 }
 console.log(sumLength("Hi","There"))
+
+
+//qstn2
+
+
+function typeContact(a,b){
+    return typeof a + typeof b;
+}
+console.log(typeContact("hello",5))
